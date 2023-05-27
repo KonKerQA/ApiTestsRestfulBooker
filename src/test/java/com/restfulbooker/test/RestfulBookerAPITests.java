b646a36f25f44c6b443a4cd4de1621ff6c7796a4
KonKerQA <koppektop228@gmail.com>
2023-05-27 14:55:52 +0500

    ref

--- src/test/java/com/restfulbooker/test/RestfulBookerAPITests.java
+++ src/test/java/com/restfulbooker/test/RestfulBookerAPITests.java
@@ -2,19 +2,18 @@ package com.restfulbooker.test;
 
 import com.restfulbooker.function.AuthFunction;
 import com.restfulbooker.function.CreateNewBookingFunction;
-import com.restfulbooker.models.booking.AllBookingModel;
 import com.restfulbooker.models.booking.BookerModel;
 import com.restfulbooker.models.login.LoginModelResponse;
 import com.restfulbooker.models.booking.NewBookerModel;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.time.Instant;
 
 import static com.restfulbooker.helper.RequestConverter.convertFileToString;
 import static com.restfulbooker.specs.JsonSpec.jsonSpec;
 import static io.restassured.RestAssured.config;
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.core.IsNull.notNullValue;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class RestfulBookerAPITests extends BaseTest {
@@ -37,24 +36,49 @@ public class RestfulBookerAPITests extends BaseTest {
     }
 
     @Test
-    void getBooking() {
+    void getBooking() throws IOException {
+        int bookingId = CreateNewBookingFunction.createBooking();
 
         BookerModel bookerModel = given()
                 .config(config)
                 .when()
-                .get("booking/311")
+                .get("booking/" + bookingId)
                 .then()
                 .log().body()
                 .statusCode(200)
                 .extract().as(BookerModel.class);
 
-        assertThat(bookerModel.getFirstname()).isEqualTo("John");
-        assertThat(bookerModel.getLastname()).isEqualTo("Smith");
-        assertThat(bookerModel.getTotalprice()).isEqualTo(111);
+        assertThat(bookerModel.getFirstname()).isEqualTo("Stellio");
+        assertThat(bookerModel.getLastname()).isEqualTo("Costo");
+        assertThat(bookerModel.getTotalprice()).isEqualTo(223);
         assertThat(bookerModel.isDepositpaid()).isEqualTo(true);
-        assertThat(bookerModel.getBookingdates().getCheckin()).isEqualTo("2018-01-01");
-        assertThat(bookerModel.getBookingdates().getCheckout()).isEqualTo("2019-01-01");
-        assertThat(bookerModel.getAdditionalneeds()).isEqualTo("Breakfast");
+        assertThat(bookerModel.getBookingdates().getCheckin()).isEqualTo("2020-01-01");
+        assertThat(bookerModel.getBookingdates().getCheckout()).isEqualTo("2021-01-01");
+        assertThat(bookerModel.getAdditionalneeds()).isEqualTo("vodka");
+    }
+
+
+    @Test
+    void getAllBookingIds() {
+
+        given()
+                .config(config)
+                .when()
+                .get("/booking")
+                .then()
+                .log().body()
+                .statusCode(200);
+    }
+    @Test
+    void getInvalidBookingIds() {
+        long invalidId = Instant.now().getEpochSecond();
+        given()
+                .config(config)
+                .when()
+                .get("/booking/" + invalidId)
+                .then()
+                .log().body()
+                .statusCode(404);
     }
 
     @Test
@@ -80,21 +104,6 @@ public class RestfulBookerAPITests extends BaseTest {
         assertThat(newbookerModel.getBooking().getBookingdates().getCheckin()).isEqualTo("2020-01-01");
         assertThat(newbookerModel.getBooking().getBookingdates().getCheckout()).isEqualTo("2021-01-01");
         assertThat(newbookerModel.getBooking().getAdditionalneeds()).isEqualTo("vodka");
-    }
-
-    //todo доделать десерелиазацию
-    @Test
-    void getAllBookingIds() {
-
-        AllBookingModel allBookingModel = given()
-                .when()
-                .get("/booking")
-                .then()
-                .log().body()
-                .statusCode(200)
-                .extract().as(AllBookingModel.class);
-
-        assertThat(allBookingModel.getAllbookingid().get(0).getBookingid()).isNotNull();
     }
 
 
@@ -128,7 +137,7 @@ public class RestfulBookerAPITests extends BaseTest {
 
 
     @Test
-    void partialUpdateBooking() throws IOException  {
+    void partialUpdateBooking() throws IOException {
         String token = AuthFunction.getAuthToken().getToken();
         int bookingId = CreateNewBookingFunction.createBooking();
         System.out.println(token);
